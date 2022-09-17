0fa26eb6bafae5aa2c1952aeb80e12f188f98904
Andy Yuen <andyyuen287@gmail.com>
2022-09-16 21:25:27 -0700

    setup initial component files

---
+++ src/components/header/QuizzlerHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./QuizzlerHeader.css";
+
+export const QuizzlerHeader = () => {
+  return (
+    <>
+      <div className="header__container">
+        <div className="nav__items">
+          <div>Quizzler</div>
+          <div>Pomodoro</div>
+          <div>Study</div>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/components/right-display/DisplayRight.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Pomodoro from "./Pomodoro";
+export const DisplayRight = () => {
+  return (
+    <>
+      <Pomodoro />
+    </>
+  );
+};
